62d025bdbf389050a8fc2ba0b586ec004215d4aa
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-09-19 14:09:04 +0900

    files_sort→files_sort_displayメソッドに、全体をリファクタリング

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,30 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def fetched_file
+require 'debug'
+
+def fetched_files
   Dir.glob('*')
 end
 
 def files_sort_display
-  files = fetched_file
+  files = fetched_files
   elements_count = files.count
   columns_count = (elements_count % 3).zero? ? elements_count / 3 : elements_count / 3 + 1
 
-  files = files_acquisition
-  col1 = files[0, row]
-  if number % 3 == 1
-    col2 = files[row, row - 1]
-    col3 = files[2 * row - 1, row - 1]
-    col2.push(' ')
-    col3.push(' ')
-  elsif number % 3 == 2
-    col2 = files[row, row]
-    col3 = files[2 * row, row]
-    col3.push(' ')
+  col1 = files.take(columns_count)
+  case elements_count % 3
+  when 1
+    col2 = files.slice(columns_count, columns_count - 1)
+    col3 = files.last(columns_count - 1)
+  when 2
+    col2 = files.slice(columns_count, columns_count)
+    col3 = files.last(columns_count - 1)
   else
-    col2 = files[row, row]
-    col3 = files[2 * row, row]
+    col2 = files.slice(columns_count, columns_count)
+    col3 = files.last(columns_count)
   end
+
+  col2.push(' ') if elements_count % 3 == 1 
+  col3.push(' ') if elements_count % 3 == 1 || elements_count % 3 == 2
 
   [col1, col2, col3].transpose.each do |index|
     puts index.map { |e| "#{e} " }.join(' ')
