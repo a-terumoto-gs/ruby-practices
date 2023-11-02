ad651508ed686d4159cfe5aab2672beb3b84a361
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-02 16:31:19 +0900

    rubocop修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,19 +6,37 @@ require 'etc'
 require 'time'
 
 def determine_option
-  detail_info = false
+  options = { detail_info: false, include_hidden_files: false, invert_order: false }
 
   OptionParser.new do |opts|
     opts.on('-l', 'detail_info') do
-      detail_info = true
+      options[:detail_info] = true
+    end
+
+    opts.on('-a', 'Include hidden files') do
+      options[:include_hidden_files] = true
+    end
+
+    opts.on('-r', 'invert_order') do
+      options[:invert_order] = true
     end
   end.parse!
 
-  detail_info
+  options
 end
 
-def fetch_files
-  Dir.glob('*')
+def fetch_files(options)
+  files = if options[:include_hidden_files]
+            Dir.glob('*', File::FNM_DOTMATCH)
+          else
+            Dir.glob('*')
+          end
+
+  if options[:invert_order]
+    files.reverse!
+  else
+    files
+  end
 end
 
 def sort_files(files)
@@ -132,11 +150,12 @@ def fetch_display_files_detail(files)
 end
 
 def execute_ls
-  if determine_option
+  options = determine_option
+  if options[:detail_info]
     puts "合計 #{calculate_total_blocks(Dir.pwd) / 2}"
-    fetch_display_files_detail(fetch_files)
+    fetch_display_files_detail(fetch_files(options))
   else
-    display_files(sort_files(fetch_files))
+    display_files(sort_files(fetch_files(options)))
   end
 end
 
