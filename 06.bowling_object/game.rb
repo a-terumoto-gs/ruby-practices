6027bd25c46eb6e4649d3b8669cdd6108c11f7d5
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-04 18:13:40 +0900

    rubocop修正

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'frame.rb'
-require_relative 'shot.rb'
+require_relative 'frame'
+require_relative 'shot'
 
 class Game
   def initialize(game_shot)
@@ -13,21 +13,20 @@ class Game
       shot = game_shots[index]
       if shot == 'X'
         @frames << Frame.new('X', '0')
-        index += 1  
+        index += 1
       else
         next_shot = game_shots[index + 1] || '0'
         @frames << Frame.new(shot, next_shot)
-        index += 2  
+        index += 2
       end
     end
   end
 
-
   def score
-    game_score = @frames.map(&:score).sum 
+    game_score = @frames.map(&:score).sum
     @frames.each_with_index do |current_frame, index|
-      break if index >= 9  
-  
+      break if index >= 9
+
       next_frame = @frames[index + 1]
       after_next_frame = @frames[index + 2]
 
@@ -36,7 +35,7 @@ class Game
       elsif current_frame.spare?
         game_score += spare_bonus(next_frame)
       else
-        game_score 
+        game_score
       end
     end
     game_score
