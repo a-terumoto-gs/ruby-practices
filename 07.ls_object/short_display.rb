cf865776ac5238415c5cefcb253427417ae7eff2
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-02-16 14:41:19 +0900

    ShortDisplayクラスのinitializeメソッドを修正

--- 07.ls_object/short_display.rb
+++ 07.ls_object/short_display.rb
@@ -2,14 +2,13 @@
 
 class ShortDisplay
   def initialize(essential_info)
-    @essential_info = essential_info
+    @files = essential_info.files
   end
 
   def display_files
-    files = @essential_info.files
     columns_count = 3
-    max_length = files.map(&:length).max
-    format_files = files.map { |file| file.ljust(max_length + 2) }
+    max_length = @files.map(&:length).max
+    format_files = @files.map { |file| file.ljust(max_length + 2) }
     format_files << nil while format_files.length % columns_count != 0
 
     rows = format_files.each_slice(format_files.length / columns_count).to_a
